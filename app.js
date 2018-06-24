fc26b694ff20abf1fe5be5bd3534b3f02b213cfc
talhanousher <talhanousher@gmail.com>
2018-06-24 18:21:05 +0500

    Update Styling and Dynamicallu Adding Todo List

---
+++ app.js
@@ -0,0 +1,51 @@
+var todoTitle = document.getElementById('todoTitle');
+var todoDetails = document.getElementById('todoDetails');
+var todos = [];
+var todoId = 0;
+var alert = document.getElementById('alert');
+alert.style.display = 'none';
+var todoList = document.getElementById('todoList');
+var todoListTitle = document.getElementById('todoListTitle');
+todoListTitle.style.display = 'none';
+function addTodo() {
+    // console.log(todoTitle.value);
+    // console.log(todoDetails.value);
+
+    if (todoTitle.value === '') {
+        alert.style.display = 'block';
+        alert.innerHTML = 'Please Add Title'
+        setTimeout(function () {
+            alert.style.display = 'none';
+        }, 3000);
+    } else {
+        if (todoDetails.value === '') {
+            alert.style.display = 'block';
+            alert.innerHTML = 'Please Add Details';
+            setTimeout(function () {
+                alert.style.display = 'none';
+            }, 3000);
+        } else {
+            var todo = {
+                title: todoTitle.value,
+                details: todoDetails.value,
+                id: todoId,
+            }
+            todos.push(todo);
+            todoId++;
+            todoListTitle.style.display = 'initial';
+            var card = document.createElement('div');
+            card.setAttribute('class', 'card');
+            var cardBody = document.createElement('div');
+            cardBody.setAttribute('class', 'card-body');
+            var cardTitle = document.createElement('h5');
+            var cardTitleText = document.createTextNode(todoTitle.value);
+            cardTitle.appendChild(cardTitleText);
+            cardBody.appendChild(cardTitle);
+            card.appendChild(cardBody);
+            todoList.appendChild(card);
+            todoTitle.value = '';
+            todoDetails.value = '';
+            console.log(todos);
+        }
+    }
+}
