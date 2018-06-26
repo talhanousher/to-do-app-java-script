43485b4d75e35ee0972ef19db0ad33d1e4bd8032
talhanousher <talhanousher@gmail.com>
2018-06-26 20:45:21 +0500

    Rendering Data

--- app.js
+++ app.js
@@ -33,19 +33,58 @@ function addTodo() {
             todos.push(todo);
             todoId++;
             todoListTitle.style.display = 'initial';
-            var card = document.createElement('div');
-            card.setAttribute('class', 'card');
-            var cardBody = document.createElement('div');
-            cardBody.setAttribute('class', 'card-body');
-            var cardTitle = document.createElement('h5');
-            var cardTitleText = document.createTextNode(todoTitle.value);
-            cardTitle.appendChild(cardTitleText);
-            cardBody.appendChild(cardTitle);
-            card.appendChild(cardBody);
-            todoList.appendChild(card);
-            todoTitle.value = '';
-            todoDetails.value = '';
-            console.log(todos);
+            displayOnDOM(todos);
+            // todoListTitle.style.display = 'initial';
+            // var card = document.createElement('div');
+            // card.addEventListener('mouseover', function () {
+            //     card.style.backgroundColor = 'rgb(224, 227, 233)';
+            // });
+            // card.addEventListener('mouseout', function () {
+            //     card.style.backgroundColor = 'white';
+            // })
+            // card.addEventListener('click', function () {
+            //     console.log(todoId);
+            // })
+            // card.setAttribute('class', 'card');
+            // var cardBody = document.createElement('div');
+            // cardBody.setAttribute('class', 'card-body');
+            // var cardTitle = document.createElement('h5');
+            // var cardTitleText = document.createTextNode(todoTitle.value);
+            // cardTitle.appendChild(cardTitleText);
+            // cardBody.appendChild(cardTitle);
+            // card.appendChild(cardBody);
+            // todoList.appendChild(card);
+            // todoTitle.value = '';
+            // todoDetails.value = '';
         }
     }
+}
+
+
+function displayOnDOM(todos) {
+    // console.log(todos);
+    while (todoList.firstChild) {
+        todoList.removeChild(todoList.firstChild);
+    }
+    for (var i = 0; i < todos.length; i++) {
+        var card = document.createElement('div');
+        card.addEventListener('click', myfunc);
+        card.myParam = todos[i].id;
+        card.setAttribute('class', 'card');
+        var cardBody = document.createElement('div');
+        cardBody.setAttribute('class', 'card-body');
+        var cardTitle = document.createElement('h5');
+        var cardTitleText = document.createTextNode(todos[i].title);
+        cardTitle.appendChild(cardTitleText);
+        cardBody.appendChild(cardTitle);
+        card.appendChild(cardBody);
+        todoList.appendChild(card);
+        todoTitle.value = '';
+        todoDetails.value = '';
+    }
+
+}
+
+function myfunc(ev) {
+    console.log(ev.target);
 }
